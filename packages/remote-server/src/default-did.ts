7dae3a8fd483a8783dfd59156ef707c3c3178b24
AlexAndrei98 <alexandrei1998@hotmail.it>
2023-04-05 12:51:30 +0100

    startup with did peer

--- packages/remote-server/src/default-did.ts
+++ packages/remote-server/src/default-did.ts
@@ -21,38 +21,47 @@ export async function createDefaultDid(options: CreateDefaultDidOptions) {
   if (!options.baseUrl) throw Error('[createDefaultDid] baseUrl is required')
 
   const hostname = parse(options.baseUrl).hostname
+  const messagingServiceEndpoint = options.baseUrl + options.messagingServiceEndpoint
 
   const serverIdentifier = await options?.agent?.didManagerGetOrCreate({
-    provider: 'did:web',
+    provider: 'did:peer',
     alias: hostname,
     options: {
-      keyType: <TKeyType>'Ed25519',
-    },
+      num_algo: 2,
+      service: {
+        id: "1",
+        type: "DIDCommMessaging", 
+        serviceEndpoint: messagingServiceEndpoint, 
+        description: "an endpoint for didcomm"
+      }
+    }
   })
+
+
   console.log('🆔', serverIdentifier?.did)
 
-  if (serverIdentifier && options.messagingServiceEndpoint) {
-    const messagingServiceEndpoint = options.baseUrl + options.messagingServiceEndpoint
+  // if (serverIdentifier && options.messagingServiceEndpoint) {
+  //   const messagingServiceEndpoint = options.baseUrl + options.messagingServiceEndpoint
 
-    console.log('📨 Messaging endpoint', messagingServiceEndpoint)
-    await options?.agent?.didManagerAddService({
-      did: serverIdentifier.did,
-      service: {
-        id: serverIdentifier.did + '#msg',
-        type: 'Messaging',
-        description: 'Handles incoming POST messages',
-        serviceEndpoint: messagingServiceEndpoint,
-      },
-    })
-    // list DIDCommMessaging service at the same endpoint
-    await options?.agent?.didManagerAddService({
-      did: serverIdentifier.did,
-      service: {
-        id: serverIdentifier.did + '#msg-didcomm',
-        type: 'DIDCommMessaging',
-        description: 'Handles incoming DIDComm messages',
-        serviceEndpoint: messagingServiceEndpoint,
-      },
-    })
-  }
+  //   console.log('📨 Messaging endpoint', messagingServiceEndpoint)
+  //   await options?.agent?.didManagerAddService({
+  //     did: serverIdentifier.did,
+  //     service: {
+  //       id: serverIdentifier.did + '#msg',
+  //       type: 'Messaging',
+  //       description: 'Handles incoming POST messages',
+  //       serviceEndpoint: messagingServiceEndpoint,
+  //     },
+  //   })
+  //   // list DIDCommMessaging service at the same endpoint
+  //   await options?.agent?.didManagerAddService({
+  //     did: serverIdentifier.did,
+  //     service: {
+  //       id: serverIdentifier.did + '#msg-didcomm',
+  //       type: 'DIDCommMessaging',
+  //       description: 'Handles incoming DIDComm messages',
+  //       serviceEndpoint: messagingServiceEndpoint,
+  //     },
+  //   })
+  // }
 }
